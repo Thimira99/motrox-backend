8b2e92ba749b94aa2ca82d7cd9a87a220e9b0702
hiru99 <hirunimjayasinghe@gmail.com>
2021-09-13 15:50:59 +0530

    'insertCusomer'

--- motrox_backend/src/main/java/com/example/demo/Controller/H_CustomerController.java
+++ motrox_backend/src/main/java/com/example/demo/Controller/H_CustomerController.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,4 +27,13 @@ public class H_CustomerController {
 	public List<H_Customer> getAllCustomers(){
 		return h_CustomerRepository.findAll();
 	}
+	
+	//create customer rest api
+		@PostMapping("/customers")
+		public H_Customer creatCustomers(@RequestBody H_Customer h_Customer) {
+			return h_CustomerRepository.save(h_Customer);
+			
+			
+		}
+		
 }
