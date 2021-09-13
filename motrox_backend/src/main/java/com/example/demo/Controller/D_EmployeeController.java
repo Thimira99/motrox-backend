c263a9b0fe72e81614df256be34ab894f3d616e3
Devangi99 <dgamage102@gmail.com>
2021-09-13 16:16:16 +0530

    'getEmployeeById'

--- motrox_backend/src/main/java/com/example/demo/Controller/D_EmployeeController.java
+++ motrox_backend/src/main/java/com/example/demo/Controller/D_EmployeeController.java
@@ -3,12 +3,15 @@ package com.example.demo.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.Exception.ResourceNotFoundException;
 import com.example.demo.Model.D_Employee;
 import com.example.demo.Repository.D_EmployeeRepository;
 
@@ -26,10 +29,18 @@ public class D_EmployeeController {
 	}
 	
 	//create employee rest api
-		@PostMapping("/employees")
-		public D_Employee createEmployee(@RequestBody D_Employee d_Employee) {
-			return employeeRepository.save(d_Employee);
-		}
+	@PostMapping("/employees")
+	public D_Employee createEmployee(@RequestBody D_Employee d_Employee) {
+		return employeeRepository.save(d_Employee);
+	}
+	
+	//get employee by id
+	@GetMapping("/employees/{id}")
+	public ResponseEntity<D_Employee> getEmployeeById(@PathVariable Long id) {
+		D_Employee d_Employee = employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee not found" + id));
+		return ResponseEntity.ok(d_Employee);
+	}
+	
 	
 
 }
