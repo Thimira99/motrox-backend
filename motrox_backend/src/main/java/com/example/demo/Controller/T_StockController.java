57cf119d4a75309f8bbf7e8bc9cac6b878c1f1a6
tharaka <d.tharakadayan@gmail.com>
2021-09-17 15:59:57 +0530

    stock-crud

--- motrox_backend/src/main/java/com/example/demo/Controller/T_StockController.java
+++ motrox_backend/src/main/java/com/example/demo/Controller/T_StockController.java
@@ -1,15 +1,27 @@
 package com.example.demo.Controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.Exception.ResourceNotFoundException;
 import com.example.demo.Model.T_StockItem;
 import com.example.demo.Repository.T_StockItemRepository;
 
+
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1")
 public class T_StockController {
@@ -21,6 +33,55 @@ public class T_StockController {
 	@GetMapping("/stockItems")
 	public List<T_StockItem>getAllstockItems(){
 		return stockRepository.findAll();
+	}	
+	
+	//create stock
+	@PostMapping("/stockItems")
+	public T_StockItem createStockItem( @RequestBody T_StockItem stockitem) {
+		return stockRepository.save(stockitem);
 	}
 	
+	//get stockItem by item code
+	@GetMapping("/stockItems/{itemcode}")
+	public ResponseEntity<T_StockItem> getTStockItemByItemcode(@PathVariable String itemcode) {
+		
+		T_StockItem t_stockitem = stockRepository.findById(itemcode)
+				.orElseThrow(()-> new ResourceNotFoundException("Item not exist with item code :" + itemcode ));
+		return ResponseEntity.ok(t_stockitem);
+	}
+	
+	//update stock item
+	
+	@PutMapping("/stockItems/{itemcode}")
+	public ResponseEntity<T_StockItem> updateStockItem(@PathVariable String itemcode,@RequestBody T_StockItem t_stockitemDetails){
+		T_StockItem t_stockitem = stockRepository.findById(itemcode)
+				.orElseThrow(()-> new ResourceNotFoundException("Item not exist with item code :" + itemcode ));
+		
+		t_stockitem.setItemcode(t_stockitemDetails.getItemcode());
+		t_stockitem.setItemname(t_stockitemDetails.getItemname());
+		t_stockitem.setQty(t_stockitemDetails.getQty());
+		t_stockitem.setReceivedqty(t_stockitemDetails.getReceivedqty());
+		t_stockitem.setSoldqty(t_stockitemDetails.getSoldqty());
+		t_stockitem.setDamageqty(t_stockitemDetails.getDamageqty());
+		t_stockitem.setReturnqty(t_stockitemDetails.getReturnqty());
+		t_stockitem.setPrice(t_stockitemDetails.getPrice());
+		
+		T_StockItem updatedStockItem = stockRepository.save(t_stockitem);
+		return ResponseEntity.ok(updatedStockItem);
+		
+	}
+	
+	//delete stock item
+	@DeleteMapping("/stockItems/{itemcode}")
+	public ResponseEntity<Map<String, Boolean>> deleteStockItem(@PathVariable String itemcode){
+		
+		T_StockItem t_stockitem = stockRepository.findById(itemcode)
+				.orElseThrow(()-> new ResourceNotFoundException("Item not exist with item code :" + itemcode ));
+		
+		stockRepository.delete(t_stockitem);
+		Map<String, Boolean> response = new HashMap<>();
+		response.put("delete", Boolean.TRUE);
+		return ResponseEntity.ok(response);
+		
+	}
 }
